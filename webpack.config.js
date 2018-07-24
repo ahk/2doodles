4e1c8531848bde953bce6518dbdc63657286cefb
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2018-07-23 23:24:41 -0700

    bill and andrew's excellent adventure fixing the build
    Co-authored-by: Bill Himmelsbach <whimmels@gmail.com>
    Co-authored-by: Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,3 @@
-module.exports = {
-  entry: './src/app.ts',
-  output: {
-    path: __dirname + '/public',
-    filename: 'build/app.js'
-  },
-  // devtool: "source-map",
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /lib\/.*\.js$/,
-        loader: 'script-loader',
-        exclude: /node_modules/
-      }
-    ]
-  }
+module.exports = env => {
+  return require(`./webpack.${env}.js`);
 }
